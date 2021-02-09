e9c8a29dd15b66e2e7673badd2772b56479eb9ed
Jeff Bond <jeffreybond@mac.com>
2021-02-09 16:27:58 -0500

    more styling to Home screen. Added text and background image

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <Layout>
       <div className="home">
-       
-          
+      <img src="https://i.imgur.com/Br17b4U.jpg"  width="100%"  height="auto"/>
+       <p className="centered">I'm Jeff Bond and I am an Atlanta based Software Developer/IT Project Manager. I have 20 years of success in Tech including software engineering, project manangement, and DevOps</p>
       </div>
     </Layout>
   )
