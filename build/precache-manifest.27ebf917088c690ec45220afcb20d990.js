a06eda31f700cc67dc1bb7b35e8f1c035e40a435
Jeff Bond <jeffreybond@mac.com>
2021-02-09 16:53:14 -0500

    one last commit before deployment

---
+++ build/precache-manifest.27ebf917088c690ec45220afcb20d990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb59f05a5d010334e41918c145ba803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ba9a35a2696cccf98a8",
+    "url": "/static/css/main.1f02a50e.chunk.css"
+  },
+  {
+    "revision": "965a4841e97394fd1ed3",
+    "url": "/static/js/2.abe80aa0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.abe80aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba9a35a2696cccf98a8",
+    "url": "/static/js/main.a43d0c9d.chunk.js"
+  },
+  {
+    "revision": "a15422c73947459aac30",
+    "url": "/static/js/runtime-main.2b4c64c5.js"
+  }
+]);
