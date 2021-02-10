49d802420a6e8dc490b70873e827298b691944de
Jeff Bond <jeffreybond@mac.com>
2021-02-09 20:57:56 -0500

    Updated Portfolio.jsx/css

--- src/screens/Portfolio/Portfolio.jsx
+++ src/screens/Portfolio/Portfolio.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import './Portfolio.css'
 import Layout from '../../components/shared/Layout/Layout'
-const Portfolio = () => {
-  return (
-    <Layout>
-      <div className="home">
-        {/* <ProductCards /> */}
-      </div>
-    </Layout>
-  );
-};
+import { getPortfolio } from '../../services/products'
+import { useParams, Link } from 'react-router-dom'
 
-export default Portfolio;
+const Portfolio = (props) => {
+
+
+    return (
+      <Layout>
+             <div className="portfolio-detail">
+                <img className="portfolio-detail-image" src="https://i.imgur.com/Mi0SmTX.jpg" width="200px" />
+                <div className="detail">
+                    <div className="name">Jeff Bond Portfolio</div>
+            <div className="description">A search engine for musicians and musical artists created in HTML,Javascript and the audioDb api</div>
+            <div></div>
+                </div>
+        </div>
+            <div className="portfolio-detail">
+                <img className="portfolio-detail-image" src="https://i.imgur.com/wmr1s2k.png" width="200px" />
+                <div className="detail">
+                    <div className="name">HipHopasaurus</div>
+            <div className="description">A search engine for musicians and musical artists created in HTML,Javascript and the audioDb api</div>
+            <div>GitHub Link</div>
+                </div>
+        </div>
+        
+        <div className="portfolio-detail">
+                <img className="portfolio-detail-image" src="https://i.imgur.com/beAsNnW.png" width="200px" />
+                <div className="detail">
+                    <div className="name">Rhyme-Line</div>
+            <div className="description">This is an desktop first  full CRUD web application that creates  a list of music artists and return information about them including artistName, albumImage, artistAlbum, albumSong, albumYear, artistTwitter, artistFacebook. Developed in React using a custom API developed in Airtable. </div>
+            <div></div>
+                </div>
+        </div>
+        
+        <div className="portfolio-detail">
+                <img className="portfolio-detail-image" src="https://i.imgur.com/ApadR7z.png" width="200px" />
+                <div className="detail">
+                    <div className="name">Pre:vue</div>
+            <div className="description">This is an desktop first  full CRUD web application that creates  a list of music artists and return information about them including artistName, albumImage, artistAlbum, albumSong, albumYear, artistTwitter, artistFacebook. Developed in React using a custom API developed in Airtable. </div>
+            <div></div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Portfolio
--- src/services/products.js
+++ src/services/products.js
@@ -1,46 +1,21 @@
 import api from './apiConfig'
 
-export const getProducts = async () => {
+export const getPortfolio = async () => {
   try {
-      const response = await api.get('/products')
+      const response = await api.get('/portfolio')
       return response.data
   } catch (error) {
       throw error
   }
 }
 
-export const getProduct = async id => {
-  try {
-      const response = await api.get(`/products/${id}`)
-      return response.data
-  } catch (error) {
-      throw error
-  }
-}
+// export const getPortfolio = async id => {
+//   try {
+//       const response = await api.get(`/portfolio/${id}`)
+//       return response.data
+//   } catch (error) {
+//       throw error
+//   }
+// }
 
-export const createProduct = async product => {
-  try {
-      const response = await api.post('/products', product)
-      return response.data
-  } catch (error) {
-      throw error
-  }
-}
 
-export const updateProduct = async (id, product) => {
-  try {
-      const response = await api.put(`/products/${id}`, product)
-      return response.data
-  } catch (error) {
-      throw error
-  }
-}
-
-export const deleteProduct = async id => {
-  try {
-      const response = await api.delete(`/products/${id}`)
-      return response.data
-  } catch (error) {
-      throw error
-  }
-}
