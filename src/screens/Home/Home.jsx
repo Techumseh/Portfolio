d4e6db10576a1d6450256b6c382cd90516e43d29
Jeff Bond <jeffreybond@mac.com>
2021-02-09 21:14:04 -0500

    made changes to home.jsx

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <Layout>
       <div className="home">
-      <img src="https://i.imgur.com/Br17b4U.jpg"  width="100%"  height="auto"/>
+      <img src="https://i.imgur.com/uTjXz8V.jpg"  width="100%"  height="auto"/>
        <p className="centered">I'm Jeff Bond and I am an Atlanta based Software Developer/IT Project Manager. I have 20 years of success in Tech including software engineering, project manangement, and DevOps</p>
       </div>
     </Layout>
