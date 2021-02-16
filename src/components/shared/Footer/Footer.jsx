660d0cf4ca122c963f149b73e0b519ea2e18b9b8
Jeff Bond <jeffreybond@mac.com>
2021-02-16 12:12:16 -0500

    made small changes to layout.css

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -7,8 +7,7 @@ const Footer = () =>
     <a href="https://www.linkedin.com/in/jeffbond/" target=" "> linkedIn </a>
     <a href="https://github.com/Techumseh" target=" "> GitHub </a>
     <a href="https://jeffrey-bond.medium.com" target=" "> Medium </a>
-    <a href="https://calendly.com/jeffrey-bond" target=" "> Calendly </a>
-    
+    <a href="https://calendly.com/jeffrey-bond" target=" "> Calendly </a> 
     </footer>
   </div>
 
