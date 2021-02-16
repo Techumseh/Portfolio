c1f9afa7fd6b6f5ffba7e3e49dff3369acb8d3b5
Jeff Bond <jeffreybond@mac.com>
2021-02-16 11:41:11 -0500

    made change to .nav in Nav.css. removed box shadow

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 // import './Footer.css'
 
 const Footer = () =>
-  <footer className="footer">
+  <div className="footer">
+  <footer >
     <a href="https://www.linkedin.com/in/jeffbond/" target=" "> linkedIn </a>
     <a href="https://github.com/Techumseh" target=" "> GitHub </a>
     <a href="https://jeffrey-bond.medium.com" target=" "> Medium </a>
     <a href="https://calendly.com/jeffrey-bond" target=" "> Calendly </a>
     
     </footer>
+  </div>
 
 export default Footer
