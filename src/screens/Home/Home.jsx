9df221a2cb9b909493deb7cfa3b616b6f3dacc4d
Jeff B <jeffreybond@mac.com>
2021-04-22 08:40:37 -0400

    Update Home.jsx
    Updated skills

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <Layout>
       <div className="home">
       <img className="background" src="https://i.imgur.com/uTjXz8V.jpg" />
-        <div className="centered">I'm Jeff Bond and I am an Atlanta based Software Developer/IT Project Manager. I have 20 years of success in Tech including full stack software engineering, project manangement, and DevOps.
+        <div className="centered">I'm Jeff Bond and I am an Atlanta based Software Developer/IT Project Manager. I have 20 years of success in Tech including full stack software engineering, project manangement, SCRUM and DevOps.
        </div>
       </div>
     </Layout>
