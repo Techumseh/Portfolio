c8f26b97d750a6900dfef4763dc927007fb76b49
Jeff Bond <jeffreybond@mac.com>
2021-05-09 20:25:06 -0400

    fixed resume.jsx layout

--- src/screens/Resume/Resume.jsx
+++ src/screens/Resume/Resume.jsx
@@ -1,75 +1,128 @@
-import React from 'react';
-import Layout from '../../components/shared/Layout/Layout'
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
 
 const Resume = () => {
   return (
     <Layout>
       <div className="home">
-      <div className="portfolio-detail">
-                
-                <div className="detail">
+        <div className="portfolio-detail">
+          <div className="detail">
             <div className="name">Jeff Bond Resume</div>
-            <a href="https://drive.google.com/file/d/1OlmQMxI2sCFhqp-oCRd1yRvB6IQPcNFX/view?usp=sharing" target="_blank">Download pdf</a>
-            <div className="description"><h2>Experience</h2></div>
+            <a
+              href="https://drive.google.com/file/d/1OlmQMxI2sCFhqp-oCRd1yRvB6IQPcNFX/view?usp=sharing"
+              target="_blank"
+            >
+              Download pdf
+            </a>
+            <div className="description">
+              <h2>Experience</h2>
+            </div>
             <div>
               <h4>General Assembly Full-time</h4>
-              <a className="description">Software Engineering Fellow<br /><br />
-Dates Employed Oct 2020 – Feb 2021<br />
-Employment Duration 5 months<br />
- New York City Metropolitan Area (Remote)<br />
-Completed a full time 500+ hour, 12-week Software Engineering Immersive program which aims to give students a well-rounded technical foundation.
-
-Languages learned: JavaScript, Ruby, HTML, CSS, SQL<br />
-Libraries/Frameworks learned: React, P5.js, Node.js, Bootstrap, Sass, jQuery, Express, Mongoose, Rails, Handlebars
-Tools/Technologies learned: Git/Github, Sublime/VSCode, RESTful APIs, MongoDB, PostgreSQL, Heroku, AWS</a><br /><br />
-              
+              <a className="description">
+                Software Engineering Fellow
+                <br />
+                <br />
+                Dates Employed Oct 2020 – Feb 2021
+                <br />
+                Employment Duration 5 months
+                <br />
+                New York City Metropolitan Area (Remote)
+                <br />
+                Completed a full time 500+ hour, 12-week Software Engineering
+                Immersive program which aims to give students a well-rounded
+                technical foundation. Languages learned: JavaScript, Ruby, HTML,
+                CSS, SQL
+                <br />
+                Libraries/Frameworks learned: React, P5.js, Node.js, Bootstrap,
+                Sass, jQuery, Express, Mongoose, Rails, Handlebars
+                Tools/Technologies learned: Git/Github, Sublime/VSCode, RESTful
+                APIs, MongoDB, PostgreSQL, Heroku, AWS
+              </a>
+              <br />
+              <br />
 
               <h4>SG Contracting</h4>
-              <a className="description">Information Technology/Project Manager<br /><br />
-Dates Employed July 2016 – July 2020<br />
-Employment Duration 5 years<br />
-Atlanta Area <br />
-
-•	Led teams across broad technical, financial and business disciplines. Focused teams on business objectives and tracked progress to ensure project milestones were completed on time, on budget and with the desired results.<br />
-•	Mitigated risk factors through careful analysis of financial and statistical data. Anticipated and managed change effectively in rapidly evolving global business environments.<br />
-•	Defined processes and tools best suited to each project. Moved between agile and waterfall approaches depending on project specifics and client goals, creating detailed project road maps, plans, schedules and work breakdown structures.
-•	Contributed to the development of the Atlanta Watershed Program Dashboard
+              <a className="description">
+                Information Technology/Project Manager
+                <br />
+                <br />
+                Dates Employed July 2016 – July 2020
+                <br />
+                Employment Duration 5 years
+                <br />
+                Atlanta Area <br />
+                • Led teams across broad technical, financial and business
+                disciplines. Focused teams on business objectives and tracked
+                progress to ensure project milestones were completed on time, on
+                budget and with the desired results.
+                <br />
+                • Mitigated risk factors through careful analysis of financial
+                and statistical data. Anticipated and managed change effectively
+                in rapidly evolving global business environments.
+                <br />• Defined processes and tools best suited to each project.
+                Moved between agile and waterfall approaches depending on
+                project specifics and client goals, creating detailed project
+                road maps, plans, schedules and work breakdown structures. •
+                Contributed to the development of the Atlanta Watershed Program
+                Dashboard
               </a>
               <h4>Benchmark Management</h4>
-              <a className="description">Information Technology/Project Manager<br /><br />
-Dates Employed May 2008 – July 2016<br />
-Employment Duration 5 years<br />
-Atlanta Area <br />
-
-Supported several End users with various network/software issues. Maintained desktop systems for WIN 2000, Windows XP, Windows 7 and OS X. Maintained regular tape backups and provided trouble shooting assistance, Set up and maintained WIN 2003 server, WIN 2008 server, OS X and Microsoft Exchange servers. Administered Oracle Contract Manger, Wordpresss Websites, Data Analytics, Search Engine Optimization, Social Media Platforms, Oracle Skire, Maximo, ARC GIS, Exchange mail Accounts and maintains Networked environment. 
+              <a className="description">
+                Information Technology/Project Manager
+                <br />
+                <br />
+                Dates Employed May 2008 – July 2016
+                <br />
+                Employment Duration 5 years
+                <br />
+                Atlanta Area <br />
+                Supported several End users with various network/software
+                issues. Maintained desktop systems for WIN 2000, Windows XP,
+                Windows 7 and OS X. Maintained regular tape backups and provided
+                trouble shooting assistance, Set up and maintained WIN 2003
+                server, WIN 2008 server, OS X and Microsoft Exchange servers.
+                Administered Oracle Contract Manger, Wordpresss Websites, Data
+                Analytics, Search Engine Optimization, Social Media Platforms,
+                Oracle Skire, Maximo, ARC GIS, Exchange mail Accounts and
+                maintains Networked environment.
               </a>
-              <a className="descripton"><br/><br />
-              <h4>Education and Credentials</h4>
-CertiProf 2021<br />
-Lead Cybersecurity Professional Certificate - LCSPC<br /><br />
-SCRUM Foundations Professional Certification - SFPC<br /><br />
-  
-Georgia Institute of Technology 2021<br />
-Professional Education Certificate in Project Management<br /><br />
-                
-General Assembly 2021<br />
-Software Engineering Bootcamp Certificate of Completion <br /><br />
-                
-Fortis College February 19, 2016<br />
-Certificate of Completion DevOps Implementation Boot Camp<br /><br />
-
-Morehouse College	1986-1990<br />
-Finance Major /Computer Science Minor <br /><br />
-
-
-
-              </a><br /><br />
-              
+              <a className="descripton">
+                <br />
+                <br />
+                <h4>Education and Credentials</h4>
+                CertiProf 2021
+                <br />
+                Lead Cybersecurity Professional Certificate - LCSPC
+                <br />
+                SCRUM Foundations Professional Certification - SFPC
+                <br />
+                <br />
+                Georgia Institute of Technology 2021
+                <br />
+                Professional Education Certificate in Project Management
+                <br />
+                <br />
+                General Assembly 2021
+                <br />
+                Software Engineering Bootcamp Certificate of Completion <br />
+                <br />
+                Fortis College February 19, 2016
+                <br />
+                Certificate of Completion DevOps Implementation Boot Camp
+                <br />
+                <br />
+                Morehouse College 1986-1990
+                <br />
+                Finance Major /Computer Science Minor <br />
+                <br />
+              </a>
+              <br />
+              <br />
             </div>
-                </div>
+          </div>
         </div>
       </div>
-
     </Layout>
   );
 };
